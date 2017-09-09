ee03ed5ca1f1d710c2c64530b4f8e0839ef1b2ad
Stjarnan <stiernstrom_3@hotmail.com>
2017-09-09 18:10:54 +0200

    Refactor
    Removed uneccessary code

--- js/app.js
+++ js/app.js
@@ -16,13 +16,12 @@ function AppViewModel() {
 
     var self = this;
 
-    function location(name, category, lat, lng, adress, website, markerId, marker, active=false) {
+    function location(name, category, lat, lng, adress, markerId, marker, active=false) {
         this.name = ko.observable(name);
         this.category = ko.observable(category);
         this.lat = ko.observable(lat);
         this.lng = ko.observable(lng);
         this.adress = ko.observable(adress);
-        this.website = ko.observable(website);
         this.markerId = ko.observable(markerId);
         this.marker = ko.observable(marker);
         this.active = ko.observable(active);
@@ -34,39 +33,37 @@ function AppViewModel() {
     self.data = ko.observableArray();
 
     // push first data to observableArray AFTER google map has been initiated
-    self.data.push(new location( 'Van Gogh Museum', 'Museum', 52.358415900, 4.881075600, 'Museumplein 6, 1071 DJ Amsterdam', '', 0, markerNum[0] ));
-    self.data.push(new location( 'Cafe De Jaren', 'Food', 52.368079, 4.895396, 'Nieuwe Doelenstraat 20, 1012 CP Amsterdam', '', 1, markerNum[1] ));
-    self.data.push(new location( 'Rijksmuseum', 'Museum', 52.360034188361645, 4.885139465332031, 'Museumstraat 1, 1071 XX Amsterdam', '', 2, markerNum[2] ));
-    self.data.push(new location( 'Royal Palace Amsterdam', 'Sights', 52.373189, 4.891319, 'Dam, 1012 HG Amsterdam', '', 3, markerNum[3] ));
-    self.data.push(new location( 'Westerpark', 'Outdoors', 52.386182, 4.877758, 'Haarlemmerweg 4, 1014 Amsterdam', '', 4, markerNum[4]));
+    self.data.push(new location( 'Van Gogh Museum', 'Museum', 52.358415900, 4.881075600, 'Museumplein 6, 1071 DJ Amsterdam', 0, markerNum[0] ));
+    self.data.push(new location( 'Cafe De Jaren', 'Food', 52.368079, 4.895396, 'Nieuwe Doelenstraat 20, 1012 CP Amsterdam', 1, markerNum[1] ));
+    self.data.push(new location( 'Rijksmuseum', 'Museum', 52.360034188361645, 4.885139465332031, 'Museumstraat 1, 1071 XX Amsterdam', 2, markerNum[2] ));
+    self.data.push(new location( 'Royal Palace Amsterdam', 'Sights', 52.373189, 4.891319, 'Dam, 1012 HG Amsterdam', 3, markerNum[3] ));
+    self.data.push(new location( 'Westerpark', 'Outdoors', 52.386182, 4.877758, 'Haarlemmerweg 4, 1014 Amsterdam', 4, markerNum[4]));
 
-    self.foursquareData = ko.observableArray([ {url: ko.observable('0')}, {url: ko.observable('1')}, {url: ko.observable('2')}, {url: ko.observable('3')}, {url: ko.observable('4')}]);
+    // This function exist to add foursquare data AFTER async api call finished from foursquareAPI function
+    function push(index){
+        self.data()[index].url(res);
+    }
 
-        // This function exist to add foursquare data AFTER async api call finished from foursquareAPI function
-        function push(index){
-            self.data()[index].url(res);
+    // Api call function for foursquare-data
+    function foursquareAPI(url, index) {
+        $.getJSON(url).done(function(result){
+            if(result.response.venue.url !== undefined){
+            res = result.response.venue.url;
+            push(index);
+            } else {
+                res = "No website available";
+                push(index);
+            }
+        }).fail( function(){
+            return alert("Something went wrong with the request, It's not you, it's us! Please try again later.");
+        });
         }
 
-        // Api call function for foursquare-data
-        function foursquareAPI(url, index) {
-            $.getJSON(url).done(function(result){
-                if(result.response.venue.url !== undefined){
-                res = result.response.venue.url;
-                push(index);
-                } else {
-                    res = "No website available";
-                    push(index);
-                }
-            }).fail( function(){
-                return alert("Something went wrong with the request, It's not you, it's us! Please try again later.");
-            });
-            }
-
-        foursquareAPI("https://api.foursquare.com/v2/venues/4a2706faf964a5208c8c1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 0);
-        foursquareAPI("https://api.foursquare.com/v2/venues/4a26ff58f964a5202a7f1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 1);
-        foursquareAPI("https://api.foursquare.com/v2/venues/4a270706f964a520bd8c1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 2);
-        foursquareAPI("https://api.foursquare.com/v2/venues/4a270706f964a520bc8c1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 3);
-        foursquareAPI("https://api.foursquare.com/v2/venues/4a27071ef964a520ff8c1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 4);
+    foursquareAPI("https://api.foursquare.com/v2/venues/4a2706faf964a5208c8c1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 0);
+    foursquareAPI("https://api.foursquare.com/v2/venues/4a26ff58f964a5202a7f1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 1);
+    foursquareAPI("https://api.foursquare.com/v2/venues/4a270706f964a520bd8c1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 2);
+    foursquareAPI("https://api.foursquare.com/v2/venues/4a270706f964a520bc8c1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 3);
+    foursquareAPI("https://api.foursquare.com/v2/venues/4a27071ef964a520ff8c1fe3?oauth_token=BAK4ZVQY1W3LR244SHG4YROS0B22KPTKC2OTRGEZTEG2GBXA&v=20170904", 4);
 
     self.search = ko.observable('');
 
